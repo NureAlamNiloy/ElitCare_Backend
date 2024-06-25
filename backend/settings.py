746f335f2eb6dd72a644e3c2c4a4af292ff4b743
Niloy74779 <mdniloy74779@gmail.com>
2024-06-25 23:45:31 +0600

    Niloy

--- backend/settings.py
+++ backend/settings.py
@@ -92,11 +92,11 @@ DATABASES = {
 }
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
 
 
 # Password validation
