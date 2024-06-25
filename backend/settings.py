8c2d86ec13ef84ee723c86d8cb067d2b3d6eaaa5
Niloy74779 <mdniloy74779@gmail.com>
2024-06-25 23:42:33 +0600

    Niloy

--- backend/settings.py
+++ backend/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL")
-EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
+EMAIL_HOST_USER = "mdniloy74779@gmail.com"
+EMAIL_HOST_PASSWORD = "xdzt vhfv zjwh gzkc"
